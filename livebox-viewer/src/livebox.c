ad2417ba103938ef51c7e6aa2a52940347ec3535
Sung-jae Park <nicesj.park@samsung.com>
2014-11-25 16:25:49 +0900

    Export API
    [model] Redwood,Kiran,B3(Wearable)
    [binary_type] AP
    [customer] Docomo/Orange/ATT/Open
    [issue#] N/A
    [problem]
    [cause]
    [solution]
    [team] HomeTF
    [request]
    [horizontal_expansion]
    
    Change-Id: Ie85a533b6c9bf02c568f55a041940716350d4f57

--- livebox-viewer/src/livebox.c
+++ livebox-viewer/src/livebox.c
@@ -5,6 +5,8 @@
 
 #include "livebox.h"
 
+#define EAPI __attribute__((visibility("default")))
+
 enum lb_type { /*!< Must have to be sync with data-provider-master */
 	_LB_TYPE_NONE = 0x0,
 	_LB_TYPE_SCRIPT,
@@ -200,392 +202,392 @@ struct livebox {
 	} cbs;
 };
 
-int livebox_init(void *disp)
+EAPI int livebox_init(void *disp)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_init_with_options(void *disp, int prevent_overwrite, double event_filter, int use_thread)
+EAPI int livebox_init_with_options(void *disp, int prevent_overwrite, double event_filter, int use_thread)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_fini(void)
+EAPI int livebox_fini(void)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_client_paused(void)
+EAPI int livebox_client_paused(void)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_client_resumed(void)
+EAPI int livebox_client_resumed(void)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-struct livebox *livebox_add(const char *pkgname, const char *content, const char *cluster, const char *category, double period, int type, ret_cb_t cb, void *data)
+EAPI struct livebox *livebox_add(const char *pkgname, const char *content, const char *cluster, const char *category, double period, int type, ret_cb_t cb, void *data)
 {
     return NULL;
 }
 
-int livebox_del(struct livebox *handler, int type, ret_cb_t cb, void *data)
+EAPI int livebox_del(struct livebox *handler, int type, ret_cb_t cb, void *data)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_set_event_handler(int (*cb)(struct livebox *handler, enum livebox_event_type event, void *data), void *data)
+EAPI int livebox_set_event_handler(int (*cb)(struct livebox *handler, enum livebox_event_type event, void *data), void *data)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-void *livebox_unset_event_handler(int (*cb)(struct livebox *handler, enum livebox_event_type event, void *data))
+EAPI void *livebox_unset_event_handler(int (*cb)(struct livebox *handler, enum livebox_event_type event, void *data))
 {
     return NULL;
 }
 
-int livebox_set_fault_handler(int (*cb)(enum livebox_fault_type, const char *, const char *, const char *, void *), void *data)
+EAPI int livebox_set_fault_handler(int (*cb)(enum livebox_fault_type, const char *, const char *, const char *, void *), void *data)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-void *livebox_unset_fault_handler(int (*cb)(enum livebox_fault_type, const char *, const char *, const char *, void *))
+EAPI void *livebox_unset_fault_handler(int (*cb)(enum livebox_fault_type, const char *, const char *, const char *, void *))
 {
     return NULL;
 }
 
-int livebox_activate(const char *pkgname, ret_cb_t cb, void *data)
+EAPI int livebox_activate(const char *pkgname, ret_cb_t cb, void *data)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_resize(struct livebox *handler, int type, ret_cb_t cb, void *data)
+EAPI int livebox_resize(struct livebox *handler, int type, ret_cb_t cb, void *data)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_click(struct livebox *handler, double x, double y)
+EAPI int livebox_click(struct livebox *handler, double x, double y)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_set_group(struct livebox *handler, const char *cluster, const char *category, ret_cb_t cb, void *data)
+EAPI int livebox_set_group(struct livebox *handler, const char *cluster, const char *category, ret_cb_t cb, void *data)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_get_group(struct livebox *handler, const char **cluster, const char **category)
+EAPI int livebox_get_group(struct livebox *handler, const char **cluster, const char **category)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-double livebox_period(struct livebox *handler)
+EAPI double livebox_period(struct livebox *handler)
 {
     return 0.0f;
 }
 
-int livebox_set_period(struct livebox *handler, double period, ret_cb_t cb, void *data)
+EAPI int livebox_set_period(struct livebox *handler, double period, ret_cb_t cb, void *data)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-enum livebox_lb_type livebox_lb_type(struct livebox *handler)
+EAPI enum livebox_lb_type livebox_lb_type(struct livebox *handler)
 {
     return LB_TYPE_INVALID;
 }
 
-int livebox_is_user(struct livebox *handler)
+EAPI int livebox_is_user(struct livebox *handler)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-const char *livebox_content(struct livebox *handler)
+EAPI const char *livebox_content(struct livebox *handler)
 {
     return NULL;
 }
 
-const char *livebox_category_title(struct livebox *handler)
+EAPI const char *livebox_category_title(struct livebox *handler)
 {
     return NULL;
 }
 
-const char *livebox_filename(struct livebox *handler)
+EAPI const char *livebox_filename(struct livebox *handler)
 {
     return NULL;
 }
 
-const char *livebox_pkgname(struct livebox *handler)
+EAPI const char *livebox_pkgname(struct livebox *handler)
 {
     return NULL;
 }
 
-double livebox_priority(struct livebox *handler)
+EAPI double livebox_priority(struct livebox *handler)
 {
     return 0.0f;
 }
 
-void *livebox_acquire_fb(struct livebox *handler)
+EAPI void *livebox_acquire_fb(struct livebox *handler)
 {
     return NULL;
 }
 
-int livebox_release_fb(void *buffer)
+EAPI int livebox_release_fb(void *buffer)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_fb_refcnt(void *buffer)
+EAPI int livebox_fb_refcnt(void *buffer)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-void *livebox_acquire_pdfb(struct livebox *handler)
+EAPI void *livebox_acquire_pdfb(struct livebox *handler)
 {
     return NULL;
 }
 
-int livebox_release_pdfb(void *buffer)
+EAPI int livebox_release_pdfb(void *buffer)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_pdfb_refcnt(void *buffer)
+EAPI int livebox_pdfb_refcnt(void *buffer)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_size(struct livebox *handler)
+EAPI int livebox_size(struct livebox *handler)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_get_pdsize(struct livebox *handler, int *w, int *h)
+EAPI int livebox_get_pdsize(struct livebox *handler, int *w, int *h)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_get_supported_sizes(struct livebox *handler, int *cnt, int *size_list)
+EAPI int livebox_get_supported_sizes(struct livebox *handler, int *cnt, int *size_list)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_lbfb_bufsz(struct livebox *handler)
+EAPI int livebox_lbfb_bufsz(struct livebox *handler)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_pdfb_bufsz(struct livebox *handler)
+EAPI int livebox_pdfb_bufsz(struct livebox *handler)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_mouse_event(struct livebox *handler, enum content_event_type type, double x, double y)
+EAPI int livebox_mouse_event(struct livebox *handler, enum content_event_type type, double x, double y)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_access_event(struct livebox *handler, enum access_event_type type, double x, double y, ret_cb_t cb, void *data)
+EAPI int livebox_access_event(struct livebox *handler, enum access_event_type type, double x, double y, ret_cb_t cb, void *data)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_key_event(struct livebox *handler, enum content_event_type type, unsigned int keycode, ret_cb_t cb, void *data)
+EAPI int livebox_key_event(struct livebox *handler, enum content_event_type type, unsigned int keycode, ret_cb_t cb, void *data)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_set_pinup(struct livebox *handler, int flag, ret_cb_t cb, void *data)
+EAPI int livebox_set_pinup(struct livebox *handler, int flag, ret_cb_t cb, void *data)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_is_pinned_up(struct livebox *handler)
+EAPI int livebox_is_pinned_up(struct livebox *handler)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_has_pinup(struct livebox *handler)
+EAPI int livebox_has_pinup(struct livebox *handler)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_has_pd(struct livebox *handler)
+EAPI int livebox_has_pd(struct livebox *handler)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_create_pd(struct livebox *handler, ret_cb_t cb, void *data)
+EAPI int livebox_create_pd(struct livebox *handler, ret_cb_t cb, void *data)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_create_pd_with_position(struct livebox *handler, double x, double y, ret_cb_t cb, void *data)
+EAPI int livebox_create_pd_with_position(struct livebox *handler, double x, double y, ret_cb_t cb, void *data)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_move_pd(struct livebox *handler, double x, double y)
+EAPI int livebox_move_pd(struct livebox *handler, double x, double y)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_destroy_pd(struct livebox *handler, ret_cb_t cb, void *data)
+EAPI int livebox_destroy_pd(struct livebox *handler, ret_cb_t cb, void *data)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_pd_is_created(struct livebox *handler)
+EAPI int livebox_pd_is_created(struct livebox *handler)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-enum livebox_pd_type livebox_pd_type(struct livebox *handler)
+EAPI enum livebox_pd_type livebox_pd_type(struct livebox *handler)
 {
     return PD_TYPE_INVALID;
 }
 
-int livebox_is_exists(const char *pkgname)
+EAPI int livebox_is_exists(const char *pkgname)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_set_text_handler(struct livebox *handler, struct livebox_script_operators *ops)
+EAPI int livebox_set_text_handler(struct livebox *handler, struct livebox_script_operators *ops)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_set_pd_text_handler(struct livebox *handler, struct livebox_script_operators *ops)
+EAPI int livebox_set_pd_text_handler(struct livebox *handler, struct livebox_script_operators *ops)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_emit_text_signal(struct livebox *handler, const char *emission, const char *source, double sx, double sy, double ex, double ey, ret_cb_t cb, void *data)
+EAPI int livebox_emit_text_signal(struct livebox *handler, const char *emission, const char *source, double sx, double sy, double ex, double ey, ret_cb_t cb, void *data)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_set_data(struct livebox *handler, void *data)
+EAPI int livebox_set_data(struct livebox *handler, void *data)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-void *livebox_get_data(struct livebox *handler)
+EAPI void *livebox_get_data(struct livebox *handler)
 {
     return NULL;
 }
 
-int livebox_subscribe_group(const char *cluster, const char *category)
+EAPI int livebox_subscribe_group(const char *cluster, const char *category)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_unsubscribe_group(const char *cluster, const char *category)
+EAPI int livebox_unsubscribe_group(const char *cluster, const char *category)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_refresh_group(const char *cluster, const char *category, int force)
+EAPI int livebox_refresh_group(const char *cluster, const char *category, int force)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_refresh(struct livebox *handler, int force)
+EAPI int livebox_refresh(struct livebox *handler, int force)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_lb_pixmap(const struct livebox *handler)
+EAPI int livebox_lb_pixmap(const struct livebox *handler)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_pd_pixmap(const struct livebox *handler)
+EAPI int livebox_pd_pixmap(const struct livebox *handler)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_acquire_pd_pixmap(struct livebox *handler, ret_cb_t cb, void *data)
+EAPI int livebox_acquire_pd_pixmap(struct livebox *handler, ret_cb_t cb, void *data)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_release_pd_pixmap(struct livebox *handler, int pixmap)
+EAPI int livebox_release_pd_pixmap(struct livebox *handler, int pixmap)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_acquire_lb_pixmap(struct livebox *handler, ret_cb_t cb, void *data)
+EAPI int livebox_acquire_lb_pixmap(struct livebox *handler, ret_cb_t cb, void *data)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_release_lb_pixmap(struct livebox *handler, int pixmap)
+EAPI int livebox_release_lb_pixmap(struct livebox *handler, int pixmap)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_set_visibility(struct livebox *handler, enum livebox_visible_state state)
+EAPI int livebox_set_visibility(struct livebox *handler, enum livebox_visible_state state)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-enum livebox_visible_state livebox_visibility(struct livebox *handler)
+EAPI enum livebox_visible_state livebox_visibility(struct livebox *handler)
 {
     return LB_VISIBLE_ERROR;
 }
 
-int livebox_set_update_mode(struct livebox *handler, int active_update, ret_cb_t cb, void *data)
+EAPI int livebox_set_update_mode(struct livebox *handler, int active_update, ret_cb_t cb, void *data)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_is_active_update(struct livebox *handler)
+EAPI int livebox_is_active_update(struct livebox *handler)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_sync_pd_fb(struct livebox *handler)
+EAPI int livebox_sync_pd_fb(struct livebox *handler)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_sync_lb_fb(struct livebox *handler)
+EAPI int livebox_sync_lb_fb(struct livebox *handler)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-const char *livebox_alt_icon(struct livebox *handler)
+EAPI const char *livebox_alt_icon(struct livebox *handler)
 {
     return NULL;
 }
 
-const char *livebox_alt_name(struct livebox *handler)
+EAPI const char *livebox_alt_name(struct livebox *handler)
 {
     return NULL;
 }
 
-int livebox_acquire_fb_lock(struct livebox *handler, int is_pd)
+EAPI int livebox_acquire_fb_lock(struct livebox *handler, int is_pd)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_release_fb_lock(struct livebox *handler, int is_pd)
+EAPI int livebox_release_fb_lock(struct livebox *handler, int is_pd)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_set_option(enum livebox_option_type option, int state)
+EAPI int livebox_set_option(enum livebox_option_type option, int state)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_option(enum livebox_option_type option)
+EAPI int livebox_option(enum livebox_option_type option)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
 
-int livebox_set_auto_launch_handler(int (*launch_handler)(struct livebox *handler, const char *appid, void *data), void *data)
+EAPI int livebox_set_auto_launch_handler(int (*launch_handler)(struct livebox *handler, const char *appid, void *data), void *data)
 {
     return LB_STATUS_ERROR_NOT_IMPLEMENTED;
 }
